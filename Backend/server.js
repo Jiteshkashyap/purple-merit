801ea9dcc9c014a001c0c18fa0f0ad00aefc4902
Jiteshkashyap <94862516+Jiteshkashyap@users.noreply.github.com>
2026-04-17 15:12:54 +0530

    update api base url

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ connectDB()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://purple-merit-psi.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
